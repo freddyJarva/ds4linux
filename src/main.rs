4fa586390d65ef692922c76ddad955f287fcd6b5
Freddy Jarva <freddy.a.jarva@gmailcom>
2022-01-04 00:49:02 +0100

    print out the touchpad and ps button triggers

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use std::time::Duration;
+use std::{
+    io::{stdout, Write},
+    time::Duration,
+};
 
 use rusb::{Context, Device, DeviceHandle, Result, UsbContext};
 
@@ -42,10 +45,22 @@ fn main() -> Result<()> {
     // Main loop
     let timeout = Duration::from_secs(1);
 
+    let mut stdout = stdout();
+    println!("01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49 50 51 52 53 54 55 56 57 58 59 60 61 62 63 64 TP PS\n");
     loop {
         let mut buf: Vec<u8> = vec![0; 64];
         handle.read_interrupt(endpoint.address, &mut buf, timeout)?;
-        println!("{:?}", buf);
+        let outstr = buf
+            .iter()
+            .map(|b| format!("{:02X}", b))
+            .collect::<Vec<String>>()
+            .join(" ");
+        let touchpad_down = buf[7] & 0x02;
+        let ps_button = buf[7] & 0x1;
+        print!("\r{}", outstr);
+        print!(" {:02X}", touchpad_down);
+        print!(" {:02X}", ps_button);
+        stdout.flush().unwrap();
     }
 
     // cleanup after use
